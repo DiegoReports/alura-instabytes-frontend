e1748a2f0cba6b102ef243277e3a3c51e9929d74
Diego Henrique <diego.reports@gmail.com>
2024-11-27 11:19:55 -0300

    Update: add config backend

--- assets/js/fetchApis.js
+++ assets/js/fetchApis.js
@@ -3,7 +3,7 @@ let { API_URL } = process.env;
 // Função para buscar os dados do endpoint
 export default async function fetchImages() {
   try {
-    const response = await fetch(process.env.API_URL); // Usando a URL importada
+    const response = await fetch("https://imersao-backend-alura-107977333356.southamerica-east1.run.app/posts"); // Usando a URL importada
     const data = await response.json();
     return data;
   } catch (error) {
